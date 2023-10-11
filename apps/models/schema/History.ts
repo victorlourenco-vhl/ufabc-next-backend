a3fb1c11a1dda3c218cb6b585cd04db5c73483c3
vinicius <vinicius@example.com>
2023-10-11 11:28:04 -0300

    feat(models): add updateUserEnrollments to method

--- apps/models/schema/History.ts
+++ apps/models/schema/History.ts
@@ -1,3 +1,4 @@
+import { addUserEnrollmentsToQueue } from '@ufabcnext/queue';
 import { History } from '@ufabcnext/types';
 import { Model, Schema, model, models } from 'mongoose';
 
@@ -20,6 +21,10 @@ historySchema.method('updateEnrollments', async function () {
 });
 
 historySchema.pre('findOneAndUpdate', async function () {
+  //why does the legacy code pass things that aren't in the schema?
+  //also, the updateUserEnrollments cron job in the legacy code doesn't use the mandatory_credits_number, limited_credits_number, free_credits_number, credits_total properties
+  //and why are we getting the values from getUpdate() instead of this.toObject({ virtuals: true })?
+  //TODO: find out what the hell is going on here
   // calls cron job here
   // const {
   //   ra,
@@ -44,8 +49,7 @@ historySchema.pre('findOneAndUpdate', async function () {
 });
 
 historySchema.post('save', async function () {
-  // config.NODE_ENV === 'prod' &&
-  //   app.agenda.now('updateUserEnrollments', this.toObject({ virtuals: true }));
+  await addUserEnrollmentsToQueue(this.toObject({ virtuals: true }));
 });
 
 export const HistoryModel: Model<History> =
